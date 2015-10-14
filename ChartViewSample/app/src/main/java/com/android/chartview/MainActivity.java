fca9c7ae1e1e62efef7522abf12c69807837b898
linwuhan <452825089@qq.com>
2015-10-14 16:55:08 +0800

    create chartview sample

---
+++ ChartViewSample/app/src/main/java/com/android/chartview/MainActivity.java
@@ -0,0 +1,215 @@
+package com.android.chartview;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.meetyou.chartview.ChartView;
+import com.meetyou.chartview.ChartViewConfig;
+import com.meetyou.chartview.KeduValue;
+import com.meetyou.chartview.PointValue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    private static final String TAG ="MainActivity" ;
+    private ChartView chartview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        chartview = (ChartView)findViewById(R.id.chartview);
+
+        int mScreenWidth = getResources().getDisplayMetrics().widthPixels;
+
+        //构造横刻度
+        List<KeduValue> listHorizontal =getHorizontalKedu();
+
+        //构造绘制点
+        List<PointValue> listPoint =getPoint();
+
+        //构造绘制区域范围的点
+        List<PointValue> listRegion = getRegion();
+
+        config(mScreenWidth,listHorizontal,listPoint,listRegion);
+    }
+
+    /**
+     * 构造横刻度
+     * @return
+     */
+    private List<KeduValue> getHorizontalKedu(){
+        List<KeduValue> list = new ArrayList<>();
+        for(int i=0;i<20;i++){
+            KeduValue pointValue = new KeduValue();
+            pointValue.display_value=i+"天月";//显示的文本
+            pointValue.value =i+"";//具体的横刻度值
+            list.add(pointValue);
+        }
+        return list;
+    }
+
+    /**
+     * 构造点
+     * @return
+     */
+    private List<PointValue> getPoint(){
+        List<PointValue> list = new ArrayList<>();
+        int temp = 1;
+        for(int i=0;i<10;i++){
+            PointValue pointValue = new PointValue();
+            //横刻度值，必须在横刻度范围内，否则无法显示
+            pointValue.horizontal_value = i+"";
+            temp++;
+            //竖刻度值，必须在竖刻度范围内，否则会取边界值
+            pointValue.verical_value=temp+"";
+            if(temp==5){
+                temp=1;
+            }
+            //显示在游标的标题
+            pointValue.title=pointValue.verical_value+"";
+            //显示在游标的子标题
+            pointValue.title_sub = "calendar";
+            list.add(pointValue);
+        }
+        return list;
+    }
+
+
+    /**
+     * 构造区域点  主要场景用于 显示 范围
+     * @return
+     */
+    private List<PointValue> getRegion(){
+        List<PointValue> list = new ArrayList<>();
+        int temp = 5;
+        //正向
+        for(int i=0;i<20;i++){
+            PointValue pointValue = new PointValue();
+            //横刻度值，必须在横刻度范围内，否则无法显示
+            pointValue.horizontal_value = i+"";
+            //竖刻度值，必须在竖刻度范围内，否则会取边界值
+            pointValue.verical_value=10+"";
+            list.add(pointValue);
+        }
+
+        for(int i=19;i>=0;i--){
+            PointValue pointValue = new PointValue();
+            //横刻度值，必须在横刻度范围内，否则无法显示
+            pointValue.horizontal_value = i+"";
+            //竖刻度值，必须在竖刻度范围内，否则会取边界值
+            pointValue.verical_value=5+"";
+            list.add(pointValue);
+        }
+        return list;
+    }
+
+
+    private void config(int mScreenWidth, List<KeduValue> listHorizontal, List<PointValue> listPoint,  List<PointValue> listPointRegion) {
+        //初始化
+        ChartViewConfig config = new ChartViewConfig();
+        config
+                //设置列数
+                .setCloumn(4)
+                //设置行数
+                .setRow(10)
+                //设置每一个格子的高度
+                .setItem_height(80)
+                //设置每一个格子的宽度
+                .setItem_width(mScreenWidth / 4)
+                //设置格子线的颜色
+                .setGrid_line_color(R.color.rcharview_grid_line_color)
+                //设置横竖刻度线的颜色
+                .setGrid_line_kedu_color(R.color.xiyou_white)
+                //设置是否显示格子线，PS:即使不显示格子线，以上的参数还是必须设置
+                .setIsShowGridLine(false)
+
+                        //设置竖向刻度左边距
+                .setVerical_kedu_leftmargin(mScreenWidth / 8)
+                        //设置竖向刻度单位文案
+                .setVerical_unit_text("KG")
+                        //设置竖向刻度开始值
+                .setVerical_unit_start(0)
+                        //设置竖向刻度结束值
+                .setVerical_unit_end(15)
+                        //设置竖向刻度增量
+                .setVerical_unit_incremetal(3)
+                        //设置竖向刻度值的类型，支持int和float
+                .setVerical_lable_value_type(0)
+                        //竖向刻度是否分段
+                .setVerical_need_to_fragment(false)
+                        //竖向刻度值颜色
+                .setVerical_unit_color(R.color.xiyou_white)
+                        //竖向刻度 文案 颜色
+                .setVerical_unit_lable_color(R.color.xiyou_white)
+                        //竖向刻度 文案 颜色
+                .setVerical_unit_lable_sub_color(R.color.xiyou_white)
+                        //竖向刻度 凸出线是否显示
+                .setVerical_kedu_line_show(true)
+
+                        //设置水平刻度
+                .setListHorizontalKeduAndValueType(listHorizontal, 0,"1")
+                        //设置贝塞尔区域
+                .setListPointRegion(listPointRegion)
+                        //设置区域颜色
+                .setRegion_color(R.color.chart_view_region)
+                        //设置点的内容
+                .setListPoint(listPoint)
+                        //是否平滑过渡，即贝塞尔曲线过度
+                .setIsSmoothPoint(true)
+                        //是否点线闭合,闭合则需要设置闭合区域颜色
+                .setIsFillPointRegion(true)
+                        //点线闭合区域颜色
+                .setRegion_connect_color(R.color.xiyou_white)
+
+                        //设置点和线的颜色
+                .setPath_line_color(R.color.xiyou_white)
+                    //保留
+                .setPoint_circle_color_interval(R.color.xiyou_white)
+                    //保留
+                .setPoint_circle_color_outside(R.color.xiyou_white)
+
+                        //游标的单位文本
+                .setIndicator_title_unit("kg")
+                       //游标颜色
+                .setIndicator_color(R.color.xiyou_white)
+                       //游标标题颜色
+                .setIndicator_title_color(R.color.xiyou_blue)
+                       //游标是否跟着曲线一起上下移动
+                .setIsIndicatorMoveWithPoint(true)
+                       //自定义游标背景
+                .setIndicatorBgRes(R.drawable.tree50)
+                      //设置游标半径，默认游标是圆形的
+                .setIndicator_radius(100)
+
+                    //默认选中位置
+                .setItemSelection(1)
+        ;
+        chartview.init(config);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        // noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,111 @@
+﻿##一、介绍
+
+ChartView是一个extends view的自定义图标控件
+主要提供了：
+    
+
+ - 图表格子颜色宽度自定义
+ - 横刻度 竖刻度 自定义范围和颜色，支持int类型和float类型
+ - 点线连接支持贝塞尔曲线连接，平滑过渡效果
+ - 点线连接支持是否绘制闭合区域，可自定义区域颜色
+ - 支持绘制区域颜色，主要用于标记正常范围的场景
+ - 支持游标
+ - 左右滑动 阻尼效果
+
+##二、使用参数如下
+使用方法只需要按如下配置响应参数，然后执行初始化即可；
+若需要更新数据，可直接使用ChartViewConfig 设置相应的方法，再ChartView.update即可实现刷新；
+
+
+                //初始化
+                ChartViewConfig config = new ChartViewConfig();config
+                //设置列数
+                .setCloumn(4)
+                //设置行数
+                .setRow(10)
+                //设置每一个格子的高度
+                .setItem_height(80)
+                //设置每一个格子的宽度
+                .setItem_width(mScreenWidth / 4)
+                //设置格子线的颜色
+                .setGrid_line_color(R.color.rcharview_grid_line_color)
+                //设置横竖刻度线的颜色
+                .setGrid_line_kedu_color(R.color.xiyou_white)
+                //设置是否显示格子线，PS:即使不显示格子线，以上的参数还是必须设置
+                .setIsShowGridLine(false)
+                
+                        //设置竖向刻度左边距
+                .setVerical_kedu_leftmargin(mScreenWidth / 8)
+                        //设置竖向刻度单位文案
+                .setVerical_unit_text("KG")
+                        //设置竖向刻度开始值
+                .setVerical_unit_start(0)
+                        //设置竖向刻度结束值
+                .setVerical_unit_end(15)
+                        //设置竖向刻度增量
+                .setVerical_unit_incremetal(3)
+                        //设置竖向刻度值的类型，支持int和float
+                .setVerical_lable_value_type(0)
+                        //竖向刻度是否分段
+                .setVerical_need_to_fragment(false)
+                        //竖向刻度值颜色
+                .setVerical_unit_color(R.color.xiyou_white)
+                        //竖向刻度 文案 颜色
+                .setVerical_unit_lable_color(R.color.xiyou_white)
+                        //竖向刻度 文案 颜色
+                .setVerical_unit_lable_sub_color(R.color.xiyou_white)
+                        //竖向刻度 凸出线是否显示
+                .setVerical_kedu_line_show(true)
+
+                        //设置水平刻度
+                .setListHorizontalKeduAndValueType(listHorizontal, 0,"1")
+                        //设置贝塞尔区域
+                .setListPointRegion(listPointRegion)
+                        //设置区域颜色
+                .setRegion_color(R.color.chart_view_region)
+                        //设置点的内容
+                .setListPoint(listPoint)
+                        //是否平滑过渡，即贝塞尔曲线过度
+                .setIsSmoothPoint(true)
+                        //是否点线闭合,闭合则需要设置闭合区域颜色
+                .setIsFillPointRegion(true)
+                        //点线闭合区域颜色
+                .setRegion_connect_color(R.color.xiyou_white)
+
+                        //设置点和线的颜色
+                .setPath_line_color(R.color.xiyou_white)
+                    //保留
+                .setPoint_circle_color_interval(R.color.xiyou_white)
+                    //保留
+                .setPoint_circle_color_outside(R.color.xiyou_white)
+
+                        //游标的单位文本
+                .setIndicator_title_unit("kg")
+                       //游标颜色
+                .setIndicator_color(R.color.xiyou_white)
+                       //游标标题颜色
+                .setIndicator_title_color(R.color.xiyou_blue)
+                       //游标是否跟着曲线一起上下移动
+                .setIsIndicatorMoveWithPoint(true)
+                       //自定义游标背景
+                .setIndicatorBgRes(R.drawable.tree50)
+                      //设置游标半径，默认游标是圆形的
+                .setIndicator_radius(100)
+
+                    //默认选中位置
+                .setItemSelection(1)
+        ;
+        chartview.init(config);
+        
+    
+效果图如下：
+
+![](http://7xnby9.com1.z0.glb.clouddn.com/sample1.png)	
+![](http://7xnby9.com1.z0.glb.clouddn.com/sample3.png)	
+----------
+
+
+若有问题，可及时联系：
+QQ:452825089
+mail:452825089@qq.com
+blog:http://iceanson.github.io
